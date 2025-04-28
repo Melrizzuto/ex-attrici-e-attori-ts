bbd52bc366cf5f6228e837d3f59919c978b2d249
Melrizzuto <melrizzuto4@gmail.com>
2025-04-28 09:58:41 +0200

    milestone 3

--- src/main.ts
+++ src/main.ts
@@ -23,3 +23,43 @@ type Actress = Person & {
   | "South Korean"
   | "Chinese";
 };
+
+function isActress(dati: unknown): dati is Actress {
+  return (
+    typeof dati === "object" && dati !== null &&
+    "id" in dati && typeof dati.id === "number" &&
+    "name" in dati && typeof dati.name === "string" &&
+    "birth_years" in dati && typeof dati.birth_years === "number" &&
+    "death_years" in dati && typeof dati.death_years === "number" &&
+    "biography" in dati && typeof dati.biography === "string" &&
+    "image" in dati && typeof dati.image === "string" &&
+    "most_famous_movies" in dati &&
+    typeof dati.most_famous_movies === "object" &&
+    dati.most_famous_movies instanceof Array &&
+    dati.most_famous_movies.length === 3 &&
+    dati.most_famous_movies.every(m => typeof m === "string") &&
+    "awards" in dati && typeof dati.awards === "string" &&
+    "nationality" in dati &&
+    dati.nationality instanceof Array &&
+    dati.nationality.length === 11 &&
+    dati.nationality.every(n => typeof n === "string")
+  )
+}
+
+async function getActress(id: number): Promise<Actress | null> {
+  try {
+    const response = await fetch(`https://boolean-spec-frontend.vercel.app/freetestapi/actresses/${id}`)
+    const dati: unknown = await response.json();
+    if (!isActress(dati)) {
+      throw new Error("Formato non valido")
+    }
+    return dati
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error("Errore nella ricezione dei dati")
+    } else {
+      console.error("Errore sconosciuto", error)
+    }
+  }
+  return null
+}
